7fadb1eedb96644d000f27e2e7189087035a32b9
nithin <nithinyadav8998@gmail.com>
2026-08-27 05:48:14 +0000

    prac

---
+++ nithin/script_tree.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+
+echo "enter the name:"
+read name
+mkdir $name
+cd $name
+touch git
+touch github
+touch shell
+touch k8s
+touch docker
+touch maven
+mkdir awscloud
+cd awscloud
+touch ec2
+touch eks
+touch routes3
+touch s3
+
